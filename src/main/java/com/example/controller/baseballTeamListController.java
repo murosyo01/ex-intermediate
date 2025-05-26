efe9e96bfef413a50c2ee67c266f9ee6a344467d
murosyo01 <murozuka75@gmail.com>
2025-05-26 09:52:01 +0900

    コントローラークラスの作成（未実装）

--- src/main/java/com/example/controller/baseballTeamListController.java
+++ src/main/java/com/example/controller/baseballTeamListController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class baseballTeamListController {
 
     @GetMapping("")
-    public String index(){
-        return ""
+    public String showList(){
+        return "showTeamList";
+    }
+
+    @PostMapping("/showDetail")
+    public String showDetail(){
+        return "showDetail";
     }
 }

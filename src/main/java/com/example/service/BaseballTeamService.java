faae2e68b1563cd79569adb0950ac56c48da8b4a
murosyo01 <murozuka75@gmail.com>
2025-05-26 10:06:52 +0900

    サービスクラスの作成

--- src/main/java/com/example/service/BaseballTeamService.java
+++ src/main/java/com/example/service/BaseballTeamService.java
@@ -1,7 +1,19 @@
 package com.example.service;
 
+import com.example.domain.BaseballTeam;
+import com.example.repository.BaseballTeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class BaseballTeamService {
+
+    @Autowired
+    private BaseballTeamRepository baseballTeamRepository;
+
+    public BaseballTeam findByTeamName(String teamName){
+        return baseballTeamRepository.findByTeamName(teamName);
+    }
 }
